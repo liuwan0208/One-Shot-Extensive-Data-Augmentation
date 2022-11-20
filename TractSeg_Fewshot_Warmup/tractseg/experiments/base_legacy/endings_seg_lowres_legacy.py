e33108e3bad432753e8415b5acc36e9b5c527e25
wanliu <liuwan0208@163.com>
2022-11-20 20:27:42 +0800

    upload

--- TractSeg_Fewshot_Warmup/tractseg/experiments/base_legacy/endings_seg_lowres_legacy.py
+++
@@ -1,9 +0,0 @@
-
-from tractseg.experiments.base_legacy.endings_seg_legacy import Config as EndingsSegConfig
-
-
-class Config(EndingsSegConfig):
-
-    DATASET = "HCP_32g"
-    RESOLUTION = "2.5mm"
-    FEATURES_FILENAME = "32g_25mm_peaks"
--- TractSeg_Fewshot_Warmup/tractseg/experiments/pretrained_models/old_2/EndingsSeg_12g90g270g_125mm_DS_DAugAll.py
+++
@@ -1,6 +0,0 @@
-import os
-from tractseg.experiments.base_legacy.endings_seg_legacy import Config as EndingsSegConfig
-
-
-class Config(EndingsSegConfig):
-    EXP_NAME = os.path.basename(__file__).split(".")[0]
